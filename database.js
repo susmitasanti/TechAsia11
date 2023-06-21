bf1af41bbaaf08a185f262eda0aa6cb8034c1948
susmitasanti <2021.susmita.santi@ves.ac.in>
2023-06-21 11:24:38 +0530

    database cred updated

--- database.js
+++ database.js
@@ -1,9 +1,9 @@
 const config = {
   db: {
     /* don't expose password or any sensitive info, done only for demo */
-    host: "localhost",
+    host: "127.0.0.1",
     user: "root",
-    password: "",
+    password: "Harshita@030712",
     database: "techasia11",
   },
   listPerPage: 10,
